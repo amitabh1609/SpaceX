5495a3d2088586e01c0293aa6b8e435f7d21e071
Amitabh Choudhury <choudhryamitabh@gmail.com>
2022-11-05 09:52:45 +0530

    Added all the files

---
+++ screens/HomeScreen.js
@@ -0,0 +1,58 @@
+import React, {useEffect,useState} from "react";
+import {View, StyleSheet,Text, SafeAreaView, FlatList} from 'react-native';
+import Article from "../components/Article";
+import axios from "axios";
+
+const HomeScreen = () => {
+    const [articles,setArticles] = useState([]);
+    const getNews = () => {
+        axios.get('https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=1a3b9ccff56d4ae1bd697ecddb8378d2',{
+            params:{
+                category: "technology",
+            }
+        })
+            .then( (response) =>{
+                // handle success
+                setArticles(response.data.articles);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+    }
+
+    useEffect(() => {
+        getNews();
+    },[]);
+
+    return(
+    <SafeAreaView style = {styles.container}>
+        <FlatList
+            data={articles}
+            renderItem = {({item}) =>
+                <Article
+                    urlToImage = {item.urlToImage}
+                    title = {item.title}
+                    description = {item.description}
+                    author = {item.author}
+                    publishedAt = {item.publishedAt}
+                    sourceName = {item.source.name}
+                    url={item.url}
+                    />}
+            keyExtractor = {(item) => item.title}
+        />
+        
+    </SafeAreaView>
+    )
+}
+
+export default HomeScreen;
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: '#fff',
+    }
+})
